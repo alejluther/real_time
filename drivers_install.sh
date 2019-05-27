4dd6f8601107d07c4b02279832e807f659ee1414
Alejandro <alucero@os3sl.com>
2019-05-27 04:55:48 -0700

    Fichero instalacion drivers 2019
    Signed-off-by: Alejandro <alucero@os3sl.com>

---
+++ drivers_install.sh
@@ -0,0 +1,5 @@
+cp /home/piuser/real_time/files/kernel_config /home/piuser/software/kernel/linux/.config
+
+cp -rp /home/piuser/real_time/files/varios /home/piuser/software/
+
+cp -rp /home/piuser/real_time/2019/DRIVERS /home/piuser/
