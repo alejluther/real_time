1ec042786e8d3d05548fedc874ed3dedf0cf883e
Alejandro Lucero <alucero@os3sl.com>
2019-05-28 02:45:35 -0700

    install changes
    Signed-off-by: Alejandro Lucero <alucero@os3sl.com>

--- 2019/DRIVERS/07/.tmp_versions/driver_char_simple_07.mod
+++
@@ -1,2 +0,0 @@
-/home/fpga/BEAGLEBONE/software/DRIVERS/07/driver_char_simple_07.ko
-/home/fpga/BEAGLEBONE/software/DRIVERS/07/driver_char_simple_07.o
--- drivers_install.sh
+++ drivers_install.sh
@@ -1,5 +1,3 @@
-cp /home/piuser/real_time/files/kernel_config /home/piuser/software/kernel/linux/.config
-
-cp -rp /home/piuser/real_time/files/varios /home/piuser/software/
-
-cp -rp /home/piuser/real_time/2019/DRIVERS /home/piuser/
+cp -rp /home/piuser/real_time/2019/DRIVERS/09/* /home/piuser/DRIVERS/09/
+cp -rp /home/piuser/real_time/2019/DRIVERS/11 /home/piuser/DRIVERS/
+cp -rp /home/piuser/real_time/2019/DRIVERS/12 /home/piuser/DRIVERS/
