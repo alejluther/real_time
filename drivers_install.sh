1de5038fdae8f7ef3e4600f9b3af96e9a8a2ef6d
Alejandro Lucero <alucero@os3sl.com>
2019-05-28 03:43:21 -0700

    Update install script
    Signed-off-by: Alejandro Lucero <alucero@os3sl.com>

--- drivers_install.sh
+++ drivers_install.sh
@@ -1,3 +1,6 @@
 cp -rp /home/piuser/real_time/2019/DRIVERS/09/* /home/piuser/DRIVERS/09/
 cp -rp /home/piuser/real_time/2019/DRIVERS/11 /home/piuser/DRIVERS/
 cp -rp /home/piuser/real_time/2019/DRIVERS/12 /home/piuser/DRIVERS/
+
+cp -rp /home/piuser/real_time/2019/DRIVERS/tools/Makefile /home/piuser/DRIVERS/tools/
+cp -rp /home/piuser/real_time/2019/DRIVERS/tools/gpio* /home/piuser/DRIVERS/tools/
