bf5bd5a4f498a1f6e0dccbbd83bbbcd7ff52fa96
Alejandro Lucero <alucero@os3sl.com>
2022-03-09 13:40:16 -0800

    new install script for embedded training 2022

--- install.sh
+++ install.sh
@@ -1,12 +1,2 @@
-sudo mv /home/piuser/RFS /home/piuser/RFS.buildroot
+mv /home/alucero/piuser/RFS.buildroot /home/piuser/
 sudo rm /var/lib/tftpboot/*
-
-cp /home/piuser/real_time/files/kernel_config /home/piuser/software/kernel/linux/.config
-
-cp -rp /home/piuser/real_time/files/varios /home/piuser/software/
-
-cd /home/piuser/software
-
-wget https://busybox.net/downloads/busybox-1.30.1.tar.bz2
-bunzip2 busybox-1.30.1.tar.bz2
-tar xvf busybox-1.30.1.tar
