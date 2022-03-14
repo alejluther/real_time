1122e63c73422934d7f16f0d9b3af7ac1c6ea42f
Alejandro Lucero <alucero@os3sl.com>
2022-03-14 00:40:55 -0700

    install embedded 2022

--- install.sh
+++ install.sh
@@ -1,2 +1,24 @@
-mv /home/alucero/piuser/RFS.buildroot /home/piuser/
-sudo rm /var/lib/tftpboot/*
+mv /home/piuser/software/u-boot.good /home/piuser/software/u-boot
+
+rm /home/piuser/software/varios/RFS.tar
+cp /home/piuser/.updates/curso_git/2022/varios/RFS.tar /home/piuser/software/varios/
+
+# Configuracion busybox: clean up
+cp /home/piuser/.updates/curso_git/2022/busybox/config /home/piuser/software/busybox/.config
+cd /home/piuser/software/busybox;make clean
+
+# Configuracion kernel : ramfilesystems ramdisk, .config de backup
+cp /home/piuser/.updates/curso_git/2022/kernel/config /home/piuser/software/kernel/linux/.config
+cp /home/piuser/.updates/curso_git/2022/kernel/zImage /home/piuser/software/kernel/linux/arch/arm/boot/
+
+# script_trace.sh  --->  fix script enbling/disabling traces
+cp /home/piuser/.updates/curso_git/2022/varios/script_trace.sh /home/piuser/software/varios/
+
+# uSD con uboot para NFS
+cp /home/piuser/software/uboot/u-boot/u-boot.bin /home/piuser/software/varios/uboot_NFS.bin
+cp /home/piuser/software/uboot/u-boot.initrd/u-boot.bin /home/piuser/software/varios/uboot_initrd.bin
+cp /home/piuser/software/uboot/u-boot.pivot_root/u-boot.bin /home/piuser/software/varios/uboot_uSD.bin
+cp /home/piuser/.updates/curso_git/2022/varios/*.patch /home/piuser/software/varios/
+
+# fdisk commands for user? Do they have uSD reader in the laptop?
+
